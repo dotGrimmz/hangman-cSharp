e94788854958bc02da4a43a337cbf6a27e83e0ef
Rakeem G <r.gordon@datagumbo.com>
2023-09-10 18:11:38 -0500

    first commit

---
+++ GameData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace my_first_c_app
+{
+    internal class GameData
+    {
+        public string selectedTheme, wordToGuess, displayValue, alreadyGuessed;
+        public int guessCount, incorrectGuessCount, incorrectGuessLimit;
+        public IDictionary<string, string> themes = new Dictionary<string, string>();
+        public char userGuess;
+        [JsonPropertyName("data")]
+        public List<string> wordOpts { get; set; }
+
+
+        public GameData()
+        {
+            themes["presidents"] = "C:\\Users\\RakeemGordon\\repos\\my_first_c_app\\data\\presidents.json";
+            themes["anime"] = "C:\\Users\\RakeemGordon\\repos\\my_first_c_app\\data\\anime.json";
+            themes["cars"] = "C:\\Users\\RakeemGordon\\repos\\my_first_c_app\\data\\cars.json";
+            selectedTheme = "";
+            wordToGuess = "";
+            displayValue = "";
+            alreadyGuessed = "";
+            incorrectGuessLimit = 8;
+            incorrectGuessCount = 0;
+            wordOpts = [];
+        }
+
+        public Dictionary<string, string> GetThemes()
+        {
+            return (Dictionary<string, string>)themes;
+        }
+
+        public int GenerateRandomNumber(int maxValue)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, maxValue);
+            return randomNumber;
+        }
+
+        public bool EndGameCondition()
+        {
+
+            if (incorrectGuessCount > incorrectGuessLimit)
+            {
+                Console.WriteLine("You Lose! Too many incorrect attempts =[");
+                return false;
+            }
+            if (!displayValue.Contains("_") && displayValue != "")
+            {
+                Console.WriteLine("Congrats on correctly guessing " + wordToGuess + "!");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using my_first_c_app;
+using System.Text.Json;
+
+internal class Program
+{
+ 
+    private static void Main()
+    {
+        GameData gameData = new();
+
+        gameData = StartGame(gameData);
+
+        gameData.displayValue = CreateDefaultDisplay(gameData.wordToGuess.Length);
+
+        while(gameData.EndGameCondition())
+         {
+
+            HandleGuess(gameData);
+            handleDisplay(gameData);
+        }
+
+        Console.WriteLine("Game Ended");
+    }
+
+    static GameData StartGame(GameData gameData)
+    {
+        Console.WriteLine("Welcome to Hangman! \nChoose your theme: \n");
+        foreach (string key in gameData.themes.Keys)
+        {
+            Console.WriteLine(key);
+        }
+
+
+        string selectedTheme = Console.ReadLine();
+
+        Console.WriteLine(" \nYou choose " + selectedTheme + "\n");
+
+        if (gameData.themes.ContainsKey(selectedTheme))
+        {
+
+            gameData.selectedTheme = selectedTheme;
+            string opts = File.ReadAllText(gameData.themes[selectedTheme]);
+            try
+            {
+                gameData = JsonSerializer.Deserialize<GameData>(opts);
+                if (gameData != null && gameData.wordOpts != null)
+                {
+
+                    int randInt = gameData.GenerateRandomNumber(gameData.wordOpts.Count);
+                    gameData.wordToGuess = gameData.wordOpts[randInt].ToUpper();
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while reading the file: " + e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("This theme is not valid. Please Select a valid theme \n");
+            StartGame(gameData);
+        }
+
+        return gameData;
+    }
+
+    static string CreateDefaultDisplay(int lengthOfWord)
+    {
+        string defaultDisplay = "";
+        for (int i = 0; i < lengthOfWord; i++)
+        {
+            defaultDisplay += "_";
+        }
+        return defaultDisplay;
+    }
+
+    static void handleDisplay(GameData gameData)
+    {
+
+        char[] updatedDisplay = gameData.displayValue.ToCharArray();
+        for (int i = 0; i < gameData.wordToGuess.Length; i++)
+        {
+            char currentLetter = gameData.wordToGuess[i];
+            if (currentLetter == gameData.userGuess)
+            {
+                updatedDisplay[i] = gameData.userGuess;
+            }
+        }
+
+        gameData.displayValue = new string(updatedDisplay);
+    }
+
+    static void HandleGuess(GameData gameData)
+    {
+        Console.WriteLine(gameData.displayValue + "\n");
+        Console.WriteLine("Guess a letter \n");
+        string current = Console.ReadLine().ToUpper();
+
+        Console.WriteLine("you choose " + current );
+
+        if (current == "QUIT")
+        {
+            Console.WriteLine("Sorry to see you go!");
+        }
+
+        if (current.Length > 1)
+        {
+            Console.WriteLine("Please enter only one letter ", gameData.alreadyGuessed);
+            HandleGuess(gameData);
+        }
+
+        bool alreadyUsed = gameData.alreadyGuessed.Contains(current);
+        bool correctGuess = gameData.wordToGuess.Contains(current);
+
+        if (alreadyUsed)
+        {
+            Console.WriteLine("You have already guessed " + current);
+            HandleGuess(gameData);
+        }
+
+        if (correctGuess)
+        {
+            Console.WriteLine("Correct!");
+            gameData.userGuess = current.ToCharArray()[0];
+            gameData.alreadyGuessed = current;
+            return;
+        }
+        Console.WriteLine("Incorrect, try again.");
+        gameData.alreadyGuessed += current;
+        gameData.incorrectGuessCount++;
+        HandleGuess(gameData);
+
+    }
+}
